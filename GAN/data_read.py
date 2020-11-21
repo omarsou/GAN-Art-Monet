565dff493fff5eb882921952a2381f48b56cdecc
Sabathier Remy <remy.sabathier@supelec.fr>
2020-11-21 16:55:52 +0100

    load images with dataloader

--- GAN/data_read.py
+++ GAN/data_read.py
@@ -1,23 +1,27 @@
 # File where we implement the pytorch dataset and dataloader
 import torch
 from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as dset
+from torchvision import transforms
 
 # Function that returns the DataLoader for the training
 
 
 def get_data_loader(path, batch_size, shuffle=True):
-    pass
 
-# Monet Painting Dataset Class
+    # Transform images as tensors
+    transformation = transforms.ToTensor()
 
+    # Image folder dataset
+    ds = dset.ImageFolder(
+        root=path,
+        transform=transformation
+    )
 
-class MonetDataset(Dataset):
-
-    def __init__(self):
-        pass
-
-    def __getitem__(self, indice):
-        pass
-
-    def __len__(self):
-        pass
+    # DataLoader class from pytorch
+    dl = DataLoader(
+        dataset=path,
+        batch_size=batch_size,
+        shuffle=shuffle,
+    )
+    return dl
